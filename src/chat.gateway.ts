d2609ab523bd22bf3ccc562fa42fca13d76047b0
LEE GUN HO <jason@grampus.co>
2023-11-15 15:58:37 +0900

    간단 채팅 : 로컬동작 8209 포트

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,9 +1,23 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatGateway {
+  @WebSocketServer()
+  server: Server;
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  // 클라이언트 직접 사용하거나, 데코레이터 안 쓸 때?
+  // handleMessage(client: any, payload: any): string {
+  //   return 'Hello world!';
+  // }
+  handleMessage(@MessageBody() message: string): void {
+    this.server.emit('message', message);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from "@nestjs/common";
+import { Logger } from '@nestjs/common';
+import { join } from 'path';
+import * as express from 'express';
 
 const logger = new Logger('main');
 
@@ -11,6 +13,9 @@ async function bootstrap() {
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
+
+  // 클라이언트 HTML 파일의 경로 설정
+  app.use(express.static(join(__dirname, '..', 'client')));
 
   const configService: ConfigService = app.get(ConfigService);
 
