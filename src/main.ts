2b8c45de3f027d9a732760f0e854a34e2aa13c8d
LEE GUN HO <jason@grampus.co>
2023-11-15 12:16:33 +0900

    Config에서 .env를 로드해 Port 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from "@nestjs/common";
+
+const logger = new Logger('main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+
+  const configService: ConfigService = app.get(ConfigService);
+
+  const port = process.env.PORT || configService.get('PORT');
+  await app.listen(port);
+
+  logger.log(
+    `App listening at ${port} pid=${process.pid} env=${process.env.NODE_ENV}`,
+  );
 }
 bootstrap();
